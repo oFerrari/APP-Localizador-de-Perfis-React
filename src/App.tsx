fe86810d39cff1dbde2713f0594a1adba428fd73
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2023-04-06 20:10:25 -0400

    💄 Configurando Ambiente

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { BaseLayout } from './layout/BaseLayout'
+import { CssBaseline } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <CssBaseline></CssBaseline>
+    <BaseLayout appBarTitle='Buscador de Perfis'>
+    
+    </BaseLayout>
       ola vc esta programando em react
     </>
   )
---
+++ src/layout/BaseLayout/index.tsx
@@ -0,0 +1,26 @@
+import { AppBar, Box, Toolbar, Typography } from '@mui/material'
+
+type BaseLayoutProps = {
+    children: React.ReactNode
+    appBarTitle: string
+}
+
+export function BaseLayout({ children, appBarTitle }: BaseLayoutProps) {
+    return (
+        <>
+            <AppBar>
+                <Toolbar>
+                    <Typography variant='h5'>{appBarTitle}</Typography>
+                </Toolbar>
+            </AppBar>
+            <Box padding={2} mt={10} sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                }}>
+                {children}
+            </Box>
+        </>
+    )
+}
