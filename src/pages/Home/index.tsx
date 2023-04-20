3bf9c1a59d6cf8351d65e957a18a48f4038b4b0c
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2023-04-19 21:49:52 -0400

    Usando Prevent Default

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import { Button, CssBaseline, TextField } from "@mui/material";
 import { BaseLayout } from "../../layout/BaseLayout";
 import { Theme } from "../../Theme/ThemeProvider";
 import {useQuery} from '@tanstack/react-query' 
-import { useRef, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import { getUser } from "../../services/api";
 
 
@@ -14,34 +14,36 @@ type User = {
 
 export function Home() {
 
-const [search,setSearch] = useState("")
+/* const [search,setSearch] = useState("")
 const inputRef = useRef(null)
 
 const handleClick = () =>{
     const username = inputRef.current
     console.log(username)
-}
+} */
 
-/* const {data,isLoading} = useQuery({
-    queryKey:["user-github"],
-    queryFn:() => {
-      getUser(search);
-    },
-  }) */
+const [userName, setUsername] = useState("")
+
+const handleSubmit = (event:FormEvent<HTMLFormElement>) =>{
+  event.preventDefault()
+    const inputUserName:HTMLInputElement = event.currentTarget.value
+}
 
     return (
         <Theme>
             <CssBaseline />
             <BaseLayout appBarTitle='Buscador de Perfis'>
-            <div>
-            <TextField value={search} ref={inputRef} id="outlined-basic" label="Buscar" variant="outlined" sx={{
-                        background: 'white',
-                        color: 'white',
-                        marginTop:'10px',
-                    }} />
+              
+            <form onSubmit={handleSubmit}>
+
+            <TextField value={userName} /* ref={inputRef} */ id="outlined-basic" label="Buscar" variant="outlined" sx={{
+              background: 'white',
+              color: 'white',
+              marginTop:'10px',
+            }} />
             
-            </div>
-            <Button onClick={handleClick} sx={{background:'#5e35b1',color:'white'}}>Consultar</Button>
+            <Button type="submit" sx={{background:'#5e35b1',color:'white'}}>Consultar</Button>
+            </form>
             </BaseLayout>
         </Theme>
     )
