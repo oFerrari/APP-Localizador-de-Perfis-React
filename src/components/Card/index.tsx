8d7c7b80de79589b786e214cc137c7c36aa97cfc
Andrei Ferrari Domingos <andrei.domingos@estudante.ifms.edu.br>
2023-04-23 12:52:37 -0300

    Adicionando os dados na API e condições e Estilizando Form Group

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,14 +1,32 @@
-import { Avatar, FormGroup, useRadioGroup } from '@mui/material'
-import {User} from '../../types/index'
+import { Avatar, Box, FormGroup, Typography, useRadioGroup } from '@mui/material'
+import { User } from '../../types/index'
+import { blueGrey, pink } from '@mui/material/colors'
 
-export function Card(dados:User){
-    return(
+export function Card(dados: User) {
+    return (
         <>
-        <FormGroup >
-        <img src={dados.avatar_url} alt="" />
-        <p>{dados.name}</p>
-        
-        </FormGroup>
+            <FormGroup sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: 'rgb(81, 45, 168)',
+                width: '500px',
+                height: '300px',
+                borderRadius: '15px',
+                color: 'white',
+
+            }}>{dados.avatar_url !== "" ? (
+                <>
+                    <img style={{ clipPath: 'circle(50%)' }} width={60} height={60} src={dados.avatar_url} alt="" />
+
+                    <p>{dados.name}</p>
+
+                    <a href={dados.html_url}>Link do Perfil</a>
+                </>
+            ) : (<Typography variant="body1"></Typography>
+            )}
+            </FormGroup>
+
         </>
     )
 }
